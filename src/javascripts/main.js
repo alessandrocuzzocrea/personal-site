3102874b3c2363882012d65b435bd936d3b664fa
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2016-09-24 22:34:15 +0200

    bootstrap parte II

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -6,37 +6,24 @@
     document.addEventListener('DOMContentLoaded', function() {
         console.log('OnReady');
 
-        //Projects
-        var n = document.querySelectorAll(".feature");
-        for (var i = 0; i < n.length; i++) {
-            n[i].addEventListener("click", function(e) {
-                console.log("you clicked " + this.id);
+        window.addEventListener('resize', setWindowSize);
 
-                // var menu = document.querySelector('.popup-overlay') // Using a class instead, see note below.
-                // menu.classList.toggle('popup-hide');
-                // menu.classList.toggle('popup-show');
+        function setWindowSize() {
+            // if($(this).height() != height){
+                // height = $(this).height();
+                console.log('resze');
 
-                var detailPanel = document.querySelector(".project-details-panel");
-                detailPanel.classList.add('show');
-                e.preventDefault();
-            });
+            // }
+
+
+            var n = document.querySelectorAll(".panel-body");
+                    for (var i = 0; i < n.length; i++) {
+                        console.log(""+n[i].offsetHeight);
+                        n[i].style.height = "460px";
+                    }
         }
 
-
-        //Projects detail panel - close button
-        var closeButton = document.querySelector(".project-details-panel-close-button");
-        closeButton.addEventListener("click", function(e) {
-
-            var detailPanel = document.querySelector(".project-details-panel");
-            detailPanel.classList.remove('show');
-            e.preventDefault();
-        });
-
-
-        // document.querySelectorAll(".show-popup")[0].addEventListener("click", function(e) {
-        // 	console.log("you clicked " + e.target);
-        // 	e.preventDefault();
-        // });
+        setWindowSize();
 
     })
 
